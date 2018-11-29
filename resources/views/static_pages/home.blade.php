822e662a261fbc7340bb18ba850e04bd9fd8ecfe
vagrant <vagrant@homestead>
2018-11-29 11:05:02 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,22 @@
 
 @extends('layouts/default')
 @section('title', '首页')
+
+
 @section('content')
-<h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Winter is Coming!</h1>
+    <p class="lead">
+      what you seen is <a href="https://powerful-escarpment-31873.herokuapp.com/">Larave_miniBlog</a>
+    </p>
+    <p>my watch begins</p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
+    </p>
+  </div>
+
+
+
+
+
 @stop
